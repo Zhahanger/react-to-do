1f161ad7e271c7f242f9032b6c7331051ae3a74a
zhahanger kydyraliyev <jkidiraliev@gmail.com>
2020-09-22 13:11:22 +0600

    added react file

---
+++ precache-manifest.94bda69e6e2434f7ab87ac5d31a0ed18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b36f8c969016cd2d62f6fdbebc05947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ec5fe31b6dcc4d9f82d",
+    "url": "/static/css/main.6703e45a.chunk.css"
+  },
+  {
+    "revision": "a89a9eea83312e0db451",
+    "url": "/static/js/2.cafcb383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cafcb383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec5fe31b6dcc4d9f82d",
+    "url": "/static/js/main.fa8f525b.chunk.js"
+  },
+  {
+    "revision": "b278acee63071185f4c1",
+    "url": "/static/js/runtime-main.ba9668b7.js"
+  }
+]);
